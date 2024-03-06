239a1153268a18b9033f0aa7b302fe423e9f7787
3º ETIM - Desenvolvimento de Sistemas <ds-etim-3@servidor.acad>
2024-03-06 15:21:56 -0300

    versão 1.0

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="css/style.css" rel="stylesheet" type="text/css">
+    <title>Blog do Carlão</title>
+</head>
+<body class="body">
+    <?php
+        // $conexao = mysqli_connect("127.0.0.1","root","");
+        // mysqli_select_db($conexao,"blog");
+        // mysqli_set_charset($conexao, "UTF8");
+
+        // $query = mysqli_query($conexao,"SELECT * FROM noticias");
+
+        // while($exibe = mysqli_fetch_array($query)){
+        //     echo $exibe[1];
+        // }
+    ?>
+    <div class="header">
+        Blog do Carlão
+    </div>
+</body>
+</html>
