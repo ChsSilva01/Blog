6177c17f77c6137899966a499b39b2cf129dbd92
CarlosDosGit <chsilva1419@gmail.com>
2024-03-07 20:14:14 -0300

    V1.5

--- index.php
+++ index.php
@@ -8,18 +8,31 @@
 </head>
 <body class="body">
     <?php
-        // $conexao = mysqli_connect("127.0.0.1","root","");
-        // mysqli_select_db($conexao,"blog");
-        // mysqli_set_charset($conexao, "UTF8");
+        $conexao = mysqli_connect("127.0.0.1","root","");
+        mysqli_select_db($conexao,"blog");
+        mysqli_set_charset($conexao, "UTF8");
 
-        // $query = mysqli_query($conexao,"SELECT * FROM noticias");
+        $query = mysqli_query($conexao,"SELECT imagens.imagem, textos.texto, textos.dat FROM noticia INNER JOIN imagens ON noticia.codigo_imagens = imagens.codigo_imagem INNER JOIN textos ON noticia.codigo_texto = textos.codigo_texto;");
 
-        // while($exibe = mysqli_fetch_array($query)){
-        //     echo $exibe[1];
-        // }
+        while($exibe = mysqli_fetch_array($query)){
+            echo $exibe[2];
+        }
     ?>
-    <div class="header">
-        Blog do Carlão
-    </div>
+    <!-- <div class="wraaper">
+        <div class="header">
+            <div><a class="text">BLOG DO CARLÃO</a></div>
+        </div>
+        <div class="notice">
+            <div class="image"></div>
+            <div class="text-notice">
+                
+            </div>
+        </div>
+        <div class="other-notices">
+            <div style="left: 5%; width: 90%; height: 40vh; background-color: #000; position: relative"></div>
+            <div style="left: 5%; width: 90%; height: 40vh; background-color: #000; position: relative"></div>
+            <div style="left: 5%; width: 90%; height: 40vh; background-color: #000; position: relative"></div>
+        </div>
+    </div> -->
 </body>
 </html>
