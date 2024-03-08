91d47937a30039bc656bfa8a74d1c0b0c07446e7
3º ETIM - Desenvolvimento de Sistemas <ds-etim-3@servidor.acad>
2024-03-08 11:16:42 -0300

    2.0

--- Blog.sql
+++ Blog.sql
@@ -1,22 +1,19 @@
 -- --------------------------------------------------------
 -- Servidor:                     127.0.0.1
--- Versão do servidor:           10.4.28-MariaDB - mariadb.org binary distribution
--- OS do Servidor:               Win64
--- HeidiSQL Versão:              12.5.0.6677
+-- Versão do servidor:           10.1.33-MariaDB - mariadb.org binary distribution
+-- OS do Servidor:               Win32
+-- HeidiSQL Versão:              9.5.0.5196
 -- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET NAMES utf8 */;
 /*!50503 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 
 -- Copiando estrutura do banco de dados para blog
-CREATE DATABASE IF NOT EXISTS `blog` /*!40100 DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci */;
+CREATE DATABASE IF NOT EXISTS `blog` /*!40100 DEFAULT CHARACTER SET latin1 */;
 USE `blog`;
 
 -- Copiando estrutura para tabela blog.imagens
@@ -24,11 +21,13 @@ CREATE TABLE IF NOT EXISTS `imagens` (
   `codigo_imagem` int(11) NOT NULL,
   `imagem` varchar(50) NOT NULL DEFAULT '',
   PRIMARY KEY (`codigo_imagem`) USING BTREE
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- Copiando dados para a tabela blog.imagens: ~1 rows (aproximadamente)
+/*!40000 ALTER TABLE `imagens` DISABLE KEYS */;
 INSERT INTO `imagens` (`codigo_imagem`, `imagem`) VALUES
-	(1, 'aaa');
+	(1, 'meliante.png');
+/*!40000 ALTER TABLE `imagens` ENABLE KEYS */;
 
 -- Copiando estrutura para tabela blog.noticia
 CREATE TABLE IF NOT EXISTS `noticia` (
@@ -40,26 +39,30 @@ CREATE TABLE IF NOT EXISTS `noticia` (
   KEY `FK_noticia_texto` (`codigo_texto`),
   CONSTRAINT `FK_noticia_imagens` FOREIGN KEY (`codigo_imagens`) REFERENCES `imagens` (`codigo_imagem`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `FK_noticia_texto` FOREIGN KEY (`codigo_texto`) REFERENCES `textos` (`codigo_texto`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- Copiando dados para a tabela blog.noticia: ~1 rows (aproximadamente)
+/*!40000 ALTER TABLE `noticia` DISABLE KEYS */;
 INSERT INTO `noticia` (`cod_noticia`, `codigo_imagens`, `codigo_texto`) VALUES
 	(1, 1, 1);
+/*!40000 ALTER TABLE `noticia` ENABLE KEYS */;
 
 -- Copiando estrutura para tabela blog.textos
 CREATE TABLE IF NOT EXISTS `textos` (
   `codigo_texto` int(11) NOT NULL AUTO_INCREMENT,
   `dat` date DEFAULT NULL,
-  `texto` varchar(500) DEFAULT NULL,
+  `texto` longtext,
+  `titulo` longtext,
+  `subtitulo` longtext,
   PRIMARY KEY (`codigo_texto`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
--- Copiando dados para a tabela blog.textos: ~1 rows (aproximadamente)
-INSERT INTO `textos` (`codigo_texto`, `dat`, `texto`) VALUES
-	(1, '2026-03-02', 'aaa');
+-- Copiando dados para a tabela blog.textos: ~0 rows (aproximadamente)
+/*!40000 ALTER TABLE `textos` DISABLE KEYS */;
+INSERT INTO `textos` (`codigo_texto`, `dat`, `texto`, `titulo`, `subtitulo`) VALUES
+	(1, '2026-03-02', 'No tranquilo bairro residencial de São Mateus, uma série de furtos misteriosos deixou os moradores perplexos nas últimas semanas. O mistério envolvendo a ração desaparecendo dos potes dos pets finalmente foi resolvido quando uma vigilância noturna inesperada pegou o verdadeiro culpado em flagrante: um cachorro local, apelidado de Peludo.', 'Cachorro Ladino: Peludo é Pego Roubando Ração de Casa Vizinha', 'Caso inusitado ocorreu no bairro quando moradores descobriram o verdadeiro "ladrão de ração" que vinha esvaziando seus potes.');
+/*!40000 ALTER TABLE `textos` ENABLE KEYS */;
 
-/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
