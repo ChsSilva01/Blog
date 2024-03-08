e13f799d1a01f52da3840ee6b375dcded85161f3
3º ETIM - Desenvolvimento de Sistemas <ds-etim-3@servidor.acad>
2024-03-08 11:00:47 -0300

    2.0

--- index.php
+++ index.php
@@ -12,27 +12,35 @@
         mysqli_select_db($conexao,"blog");
         mysqli_set_charset($conexao, "UTF8");
 
-        $query = mysqli_query($conexao,"SELECT imagens.imagem, textos.texto, textos.dat FROM noticia INNER JOIN imagens ON noticia.codigo_imagens = imagens.codigo_imagem INNER JOIN textos ON noticia.codigo_texto = textos.codigo_texto;");
+        $query = mysqli_query($conexao,"SELECT imagens.imagem, textos.texto, textos.dat, textos.titulo, textos.subtitulo FROM noticia INNER JOIN imagens ON noticia.codigo_imagens = imagens.codigo_imagem INNER JOIN textos ON noticia.codigo_texto = textos.codigo_texto;");
 
         while($exibe = mysqli_fetch_array($query)){
-            echo $exibe[2];
+            echo 
+            '<div class="wraaper">
+                <div class="header">
+                    <div><a class="text">BLOG DO CARLÃO</a></div>
+                </div>
+                <div class="notice">
+                    <div class="image">
+                        <img src="./img/'.$exibe[0].'" style= "width: 100%; border-radius: 1vw; height: 100%">
+                    </div>
+                    <div class="text-notice">
+                        <b style="font-size: 2vw">'.$exibe[3].'</b><br>
+                        <a style="font-size: 1vw">'.$exibe[4].'</a><br>
+                        <a style="font-size: 1vw; padding-top: 2vw">'.$exibe[1].'</a>
+                    </div>
+                </div>
+                <div class="other-notices">
+                    <div class="information">
+                        
+                    </div>
+                    <div class="information"></div>
+                    <div class="information"></div>
+                </div>
+            </div>'      
+            ;
         }
     ?>
-    <!-- <div class="wraaper">
-        <div class="header">
-            <div><a class="text">BLOG DO CARLÃO</a></div>
-        </div>
-        <div class="notice">
-            <div class="image"></div>
-            <div class="text-notice">
-                
-            </div>
-        </div>
-        <div class="other-notices">
-            <div style="left: 5%; width: 90%; height: 40vh; background-color: #000; position: relative"></div>
-            <div style="left: 5%; width: 90%; height: 40vh; background-color: #000; position: relative"></div>
-            <div style="left: 5%; width: 90%; height: 40vh; background-color: #000; position: relative"></div>
-        </div>
-    </div> -->
+    
 </body>
 </html>
